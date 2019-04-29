5d9dc44de2d74491ee729873081ed6135611a6dd
user <exitstop@list.ru>
2019-04-29 18:24:05 +0300

    build script;

--- compile.sh
+++ compile.sh
@@ -1,5 +1,32 @@
-rm -rf build_dbg
-mkdir -p build_dbg
-cd build_dbg
-cmake -GNinja .. -DCMAKE_BUILD_TYPE=Debug
-ninja
+#!/bin/bash
+
+function clean {
+    rm -R -f debug
+    rm -R -f release
+}
+
+function build {
+    build_type=$1
+    lowercase_build_type=$(echo ${build_type} | tr '[:upper:]' '[:lower:]')
+    mkdir -p $lowercase_build_type
+    cd $lowercase_build_type
+    cmake -GNinja .. -DCMAKE_BUILD_TYPE=$build_type
+    ninja
+    cd ..
+}
+
+if [ $# -eq 0 ]; then
+    build Debug
+    build Release
+elif [ "$1" = "clean" ]; then
+    clean
+elif [ "$1" = "debug" ]; then
+    build Debug
+elif [ "$1" = "release" ]; then
+    build Release
+elif [ "$1" = "all" ]; then
+    build Debug
+    build Release
+else
+    echo "build.sh <clean|all|debug|release|help>"
+fi
--- main/myLib.cpp
+++ main/myLib.cpp
@@ -3,44 +3,38 @@
 // декомпилированный код из crackme2
 uint32_t keygenAlgo(uint16_t* a1)
 {
-    uint16_t v2;
-    int8_t* v3;
-    uint32_t ecx4;
-    uint32_t ecx5;
-    int8_t* v6;
-    uint32_t ecx7;
-    uint32_t ecx8;
-    uint32_t ecx9;
-    uint32_t ecx10;
-    uint32_t ecx11;
-    uint32_t eax12;
+    int8_t* tCross;
+    int8_t* bt;
+    uint16_t counter;
+    uint32_t shlCross;
+    uint32_t crossRoad;
+    uint32_t shlBt;
+    uint32_t tm1;
+    uint32_t testEnd;
 
-    v2 = 0;
+    counter = 0;
     while (1) {
-        v3 = (int8_t*)((int32_t)a1 + ((int32_t)static_cast<uint32_t>(*(uint16_t*)((int32_t)a1 + (static_cast<uint32_t>(v2) << 3))) >> 3));
-        ecx4 = static_cast<uint32_t>(*(uint16_t*)((int32_t)a1 + (static_cast<uint32_t>(v2) << 3))) & 7;
-        ecx5 = (uint32_t)static_cast<int32_t>(*v3) & 1 << *(uint8_t*)&ecx4;
-        v6 = (int8_t*)((int32_t)a1 + ((int32_t)static_cast<uint32_t>(*(uint16_t*)((int32_t)a1 + (static_cast<uint32_t>(v2) << 3) + 2)) >> 3));
-        if (!*(int8_t*)&ecx5) {
-            ecx7 = static_cast<uint32_t>(*(uint16_t*)((int32_t)a1 + (static_cast<uint32_t>(v2) << 3) + 2)) & 7;
-            ecx8 = (uint32_t)static_cast<int32_t>(*v6) & (1 << *(uint8_t*)&ecx7 ^ 0xffffffff);
-            *v6 = *(int8_t*)&ecx8;
+        tCross = (int8_t*)((int32_t)a1 + ((int32_t)(*(uint16_t*)((int32_t)a1 + ((uint32_t)(counter) << 3))) >> 3));
+        shlCross = (uint32_t)(*(uint16_t*)((int32_t)a1 + ((uint32_t)(counter) << 3))) & 7;
+        crossRoad = (uint32_t)(*tCross) & 1 << *(uint8_t*)&shlCross;
+        bt = (int8_t*)((int32_t)a1 + ((int32_t)(uint32_t)(*(uint16_t*)((int32_t)a1 + ((uint32_t)(counter) << 3) + 2)) >> 3));
+        if (!*(int8_t*)&crossRoad) {
+            shlBt = (uint32_t)(*(uint16_t*)((int32_t)a1 + ((uint32_t)(counter) << 3) + 2)) & 7;
+            tm1 = (uint32_t)(int32_t)(*bt) & (1 << *(uint8_t*)&shlBt ^ 0xffffffff);
+            *bt = *(int8_t*)&tm1;
         } else {
-            ecx9 = static_cast<uint32_t>(*(uint16_t*)((int32_t)a1 + (static_cast<uint32_t>(v2) << 3) + 2)) & 7;
-            ecx10 = (uint32_t)static_cast<int32_t>(*v6) | 1 << *(uint8_t*)&ecx9;
-            *v6 = *(int8_t*)&ecx10;
+            shlBt = (uint32_t)(*(uint16_t*)((int32_t)a1 + ((uint32_t)(counter) << 3) + 2)) & 7;
+            tm1 = (uint32_t)(int32_t)(*bt) | 1 << *(uint8_t*)&shlBt;
+            *bt = *(int8_t*)&tm1;
         }
-        ecx11 = v2 + (*(uint16_t*)((int32_t)a1 + (static_cast<uint32_t>(v2) << 3) + 4) + 1);
-        v2 = *(uint16_t*)&ecx11;
-        if (static_cast<uint32_t>(v2) == 0xffff) 
+        testEnd = counter + (*(uint16_t*)((int32_t)a1 + ((uint32_t)(counter) << 3) + 4) + 1);
+        counter = *(uint16_t*)&testEnd;
+        if ((uint32_t)(counter) == 0xffff) {
             break;
-        eax12 = static_cast<uint32_t>(v2);
-        if (eax12 > 0x601) 
-            goto addr_0x401174_7;
+        }
+        if (counter > 0x601) {
+            return counter;
+        }
     }
-    eax12 = 0;
-addr_0x40117b_10:
-    return eax12;
-addr_0x401174_7:
-    goto addr_0x40117b_10;
+    return 0;
 }
